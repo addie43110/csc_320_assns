ad3d0d4230ddaa078f4a7ea088289383ef0bebb1
addie <aedjordon@gmail.com>
2018-10-06 15:50:59 -0700

    completed up to 6: get_nth

--- ass1.scala
+++ ass1.scala
@@ -6,7 +6,108 @@ package ass1
 
 object ass1 {
   case class Date(day: Int, month: Int, year: Int)
+  final case class InvalidParameterException(private val msg: String="",
+      private val cause: Throwable = None.orNull) extends Exception(msg, cause)      
 
+/* 1.  Write a function is_older that takes two dates and evaluates to true or false.
+      It evaluates to true if the first argument is a date that comes before the second argument.
+      (If the two dates are the same, the result is false.)
+
+  2.  Write a function number_in_month that takes a list of dates and a month (i.e., an int)
+      and returns how many dates in the list are in the given month.
+
+
+  3.  Write a function number_in_months that takes a list of dates and a list of months (i.e., an
+      int list) and returns the number of dates in the list of dates that are 
+      in any of the months in the list of months. Assume the list of months has no number
+      repeated.  Hint:  Use your answer to the previous problem.
+
+
+  4.  Write a function dates_in_month that takes a list of dates and a month (i.e., an int)
+      and returns a list holding the dates from the argument list of dates that are 
+      in the month.  The returned list should contain dates in the order they were originally given.
+
+
+  5.  Write a function dates_in_months that takes a list of dates and a list of months (i.e., an
+      int list) and returns a list holding the dates from the argument list of dates
+      that are in any of the months in the list of months.  Assume the list of
+      months has no number repeated.  Hint:  Use your answer to the previous problem and 
+      ML’s list-append operator (@).
+
+
+  6.  Write a function get_nth that takes a list of strings and an int n and returns 
+      the n-th element of the list where the head of the list is 1st .  Raise the exception
+      InvalidParameter if n is zero or larger than the length of the list.
+
+
+  7.  Write  a  function date_to_string that  takes  a  date  and  returns  a  string  of  the  form
+      January 20, 2013 (for  example).   Use  the  operator ˆ for  concatenating  strings 
+      and  the  library  function Int.toString for converting an int to a string.
+      For producing the month part, do not use a bunch of conditionals.  Instead, use a list
+      holding 12 strings and your answer to the previous problem.   For  consistency,
+      put  a  comma  following  the  day  and  use  capitalized  English  month  names:
+      January, February, March, April, May, June, July, August, September, October, November, December.
+
+
+  8.  Write a function number_before_reaching_sum
+      that takes an int called sum, which you can
+      assume is positive,  and an int list,
+      which you can assume contains all positive numbers,
+      and returns an int. You should return an int n
+      such that the first n elements of the list add to less than
+      sum, but the first n + 1 elements of the list add to
+      sum or more.  Assume the entire list sums to more
+      than the passed in value; it is okay for an exception
+      to occur if this is not the case.
+
+
+  9.  Write a function what_month
+      that takes a day of year (i.e., an
+      int between 1 and 365) and returns
+      what month that day is in (1 for January,
+      2 for February, etc.). Use a list
+      holding 12 integers and your
+      answer to the previous problem.
+
+
+  10. Write a function month_range
+      that takes two days of the year
+      day1 and day2 and returns an
+      int list [m1,m2,...,mn]
+      where m1 is the month of
+      day1, m2 is the month of
+      day1+1 , ..., and mn is
+      the month of day day2.
+      Note the result will have length
+      day2−day1 + 1 or length 0 if
+      day1> day2.
+
+
+  11. Write a function oldest
+      that takes a list of dates and
+      evaluates to a DATE option.  It evaluates to
+      NONE if the list has no dates and
+      SOME d if the date d is the oldest date
+      in the list.
+
+
+  12. Write a function reasonable_date
+      that takes a date and determines if
+      it describes a real date in
+      the common era.  A “real date” has a
+      positive year (year 0 did not exist),
+      a month between 1 and 12, and a day
+      appropriate for the month.  Solutions
+      should properly handle leap years. 
+      Leap years are years that are either
+      divisible by 400 or divisible by 4 but
+      not divisible by 100.  (Do not worry about
+      days possibly lost in the conversion to
+      the Gregorian calendar in the Late 1500s.)
+
+*/
+
+  /* 1 */
   def is_older(d1: Date, d2: Date): Boolean = {
     if (d1.year < d2.year)
       true
@@ -18,9 +119,39 @@ object ass1 {
       false
   }
 
-
-  def number_in_month(dt_lst: Array[Date], month: Int): Int = {
+  /* 2 */
+  def number_in_month(dt_lst: List[Date], month: Int): Int = {
     dt_lst.foldLeft(0)((a: Int, b: Date) => if (b.month==month) 1+a else a)
   }
+
+
+
+  /* 3 */
+  def number_in_months(dt_lst: List[Date], month_lst: List[Int]): List[Int] = {
+    month_lst.foldLeft(List[Int]())((a: List[Int], b: Int) => a:::List(number_in_month(dt_lst, b)))
+  }
+
+
+
+  /* 4 */
+  def dates_in_month(dt_list: List[Date], month: Int): List[Date] = {
+    dt_list.foldLeft(List[Date]())((acc: List[Date], x: Date) => if (x.month==month) acc:::List(x) else acc)
+  }
+
+
+  /* 5 */
+  def dates_in_months(dt_list: List[Date], month_list: List[Int]): List[Date] = {
+    month_list.foldLeft(List[Date]())((acc: List[Date], m: Int) => acc:::dates_in_month(dt_list, m))
+  }
+
+
+  /* 6 */
+  def get_nth(s_list: List[String], n: Int): String = {
+    if (n<1 || n>s_list.length)
+      throw InvalidParameterException("get_nth given invalid index")    
+    else
+      s_list(n-1)
+  }
+
 
 }
--- ass1tests.scala
+++ ass1tests.scala
@@ -26,9 +26,9 @@ object ass1tests {
     println(s"test3: ${test3==false}")
     println(s"test4: ${test4==false}") 
 
-    val jan_array = Array(jan_1_2018, jan_1_2016)
-    val june_array = Array(june_1_2018, june_1_2015)
-    val jan_and_june = Array(jan_1_2018, june_1_2018, june_1_2015, jan_1_2016)
+    val jan_array = List(jan_1_2018, jan_1_2016)
+    val june_array = List(june_1_2018, june_1_2015)
+    val jan_and_june = List(jan_1_2018, june_1_2018, june_1_2015, jan_1_2016)
 
     println("\n\ntesting number_in_month...")
     println(s"test5: ${ass1.number_in_month(jan_array, 1)==2}")
@@ -36,5 +36,41 @@ object ass1tests {
     println(s"test7: ${ass1.number_in_month(june_array, 1)==0}")
     println(s"test8: ${ass1.number_in_month(june_array, 6)==2}")
     println(s"test9: ${ass1.number_in_month(jan_and_june, 1)==2}")
-  }
+
+
+    println("\n\ntesting number_in_months...")
+    println(s"test10: ${ass1.number_in_months(jan_and_june, List(1)) == List(2)}")
+    println(s"test11: ${ass1.number_in_months(jan_and_june, List(1,6)) == List(2,2)}")  
+    println(s"test12: ${ass1.number_in_months(jan_and_june, List(1,2)) == List(2,0)}")
+
+
+    println("\n\ntesting dates_in_month...")
+    println(s"test13: ${ass1.dates_in_month(jan_and_june, 1) == List(ass1.Date(1,1,2018),ass1.Date(1,1,2016))}")
+    println(s"test14: ${ass1.dates_in_month(jan_and_june, 3) == List[ass1.Date]()}")
+
+
+    println("\n\ntesting dates_in_months...")
+    println(s"test15: ${ass1.dates_in_months(jan_and_june, List(1)) == List(ass1.Date(1,1,2018),ass1.Date(1,1,2016))}")
+    println(s"test16: ${ass1.dates_in_months(jan_and_june, List(2,3)) == List[ass1.Date]()}")
+    println(s"test17: ${ass1.dates_in_months(jan_and_june, List(1,6)) == List(ass1.Date(1,1,2018),ass1.Date(1,1,2016),ass1.Date(1,6,2018),ass1.Date(1,6,2015))}")
+
+
+    val sl1 = List("Hello", "Hi", "Howdy", "Bonjour", "Terve", "Moi")
+    val sl2 = List("No", "Non", "Nope", "Nah")
+    val sl3 = List("Addie", "Adeline", "Ade")
+    val sl4 = List("Washua", "Joshua", "Josh", "Jash", "Seabroo", "Seabro")
+
+    println("\n\ntesting get_nth...")
+    println(s"test18: ${try{ass1.get_nth(sl1, 2)=="Hi"}catch{case e: Exception => "False"}}")
+    println(s"test19: ${try{ass1.get_nth(sl2, 0)}catch{case e: Exception => "True"}}")
+    println(s"test20: ${try{ass1.get_nth(List[String](), 1)}catch{case e: Exception => "True"}}")
+    println(s"test21: ${try{ass1.get_nth(sl4, 20)}catch{case e: Exception =>"True"}}")
+    println(s"test22: ${try{ass1.get_nth(sl4, 4)=="Jash"}catch{case e: Exception => "False"}}")
+
+
+
+  } /* end main */
+
+
+
 }
